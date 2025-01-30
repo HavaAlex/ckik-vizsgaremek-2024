6de7c0b44de1e0aca57c002438aee5287c495bff
HavaAlex <havasi.alex.marton@gmail.com>
2025-01-30 11:51:43 +0100

    uzenetek megjelenitese rendbe van, kuldeni ugyan meg nemm lehet

--- paholy/src/api/uzenetek/uzenetekQuery.ts
+++ paholy/src/api/uzenetek/uzenetekQuery.ts
@@ -4,6 +4,7 @@ import { useRoute, useRouter } from "vue-router"
 import { QUERY_KEYS } from "@/utils/QueryKeys"
 import { jwtDecode } from "jwt-decode";
 import { useCookieHandler } from "@/stores/cookieHandler";
+
 import type { Message } from "./uzenetek";
 
 const getUzenetek = async (): Promise<Message> => {
@@ -14,6 +15,7 @@ const getUzenetek = async (): Promise<Message> => {
     //console.log(`localhost:3000/uzenetek/${document.cookie}`)
     const response = await axiosClient.get(`http://localhost:3000/paholy/uzenetek`,config) // ${document.cookie}
     console.log(response)
+    console.log(response.data)  
     return response.data
 }
 export const useGetUzenetek = () => {
--- paholy/src/views/tanar/UzenetekView.vue
+++ paholy/src/views/tanar/UzenetekView.vue
@@ -1,7 +1,47 @@
 <script setup lang="ts">
+  import { ref } from 'vue'
+  import type {Message } from '@/api/uzenetek/uzenetek';
+  import {useGetUzenetek} from '@/api/uzenetek/uzenetekQuery';
+const dialog = ref(false)
+
+
+const {data} = useGetUzenetek()
 </script>
 
 <template>
   <main>
+
+        <v-dialog max-width="500">
+      <template v-slot:activator="{ props: activatorProps }">
+        <v-btn
+          v-bind="activatorProps"
+          color="surface-variant"
+          text="Üzenet írása"
+          variant="flat"
+        ></v-btn>
+      </template>
+
+      <template v-slot:default="{ isActive }">
+        <v-card title="Üzenet">
+          <v-card-text>
+            <v-textarea label="Label"></v-textarea>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn
+              text="Mégse"
+              @click="isActive.value = false"
+            ></v-btn>
+            <v-btn
+              text="Űzenet elküldése"
+              @click="isActive.value = false"
+            ></v-btn>
+          </v-card-actions>
+        </v-card>
+      </template>
+    </v-dialog>
+
   </main>
 </template>
