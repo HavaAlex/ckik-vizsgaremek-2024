0ecf1f4f07f3a172301e505e379f203c7517ebc6
HavaAlex <havasi.alex.marton@gmail.com>
2025-02-07 15:58:42 +0100

    üzenetek layoutja szuper szexi de még 1-2 dolog hiányzik

--- paholy/src/layout/UzenetekLayout.vue
+++ paholy/src/layout/UzenetekLayout.vue
@@ -11,49 +11,101 @@ const openDialog = (uzenet: Message) => {
   selectedMessage.value = uzenet;
   dialog.value = true;
 };
+
+const Beerkezett = ref(true);
+
+const ChangeBeerkezett = () => {
+  Beerkezett.value = !Beerkezett.value;
+  
+};
+
 </script>
 
 <template>
-  <main>
-    <h1>Beérkezett üzenetek</h1>
+  <main class="main">
+    <v-navigation-drawer theme="dark" class="navigationdrawer">
+      <v-btn class="switchBtn "  id="switchBtn1" :disabled="Beerkezett"@click="ChangeBeerkezett()">Beérkezett üzenetek <br> megnézése</v-btn>
+      <v-btn class="switchBtn" id="switchBtn2" :disabled="!Beerkezett" @click="ChangeBeerkezett()">Elküldött üzenetek <br> megnézése</v-btn>
+    </v-navigation-drawer>
+    <div v-if="Beerkezett">
+      <h1>Beérkezett üzenetek</h1>
+      <v-table theme="dark"  fixed-header style="border-radius: 3%;" class="messageTable">
+        <thead>
+          <tr>
+            <th class="text-center" style="width: 15vw; justify-content: center;">Feladó</th>
+            <th class="text-center" style="width: 15vw; justify-content: center;">Dátum</th>
+            <th class="text-center" style="width: 15vw; justify-content: center;">Üzenet</th>
+            <th class="text-center" style="width: 15vw; justify-content: center;">Interakció</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="uzenet in data" ><!--:key="uzenet.id"-->
+            <td style="width: 15vw; justify-content: center;">{{ uzenet.senderUserID }}</td>
+            <td style="width: 15vw; justify-content: center;">{{ uzenet.date }}</td>
+            <td id="szoveg" style="width: 15vw; justify-content: center;">{{ uzenet.message }}</td>
+            <td style="width: 15vw; justify-content: center;">
+              <v-btn @click="openDialog(uzenet)">Megtekintés</v-btn>
+            </td>
+          </tr>
+        </tbody>
+      </v-table>
 
-    <v-table theme="dark" height="40vw" width="60vw" fixed-header style="border-radius: 3%;">
-      <thead>
-        <tr>
-          <th class="text-center">Feladó</th>
-          <th class="text-center">Dátum</th>
-          <th class="text-center">Üzenet</th>
-          <th class="text-center">Interakció</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="uzenet in data" ><!--:key="uzenet.id"-->
-          <td style="width: 15vw;">{{ uzenet.senderUserID }}</td>
-          <td style="width: 15vw;">{{ uzenet.date }}</td>
-          <td id="szoveg">{{ uzenet.message }}</td>
-          <td>
-            <v-btn @click="openDialog(uzenet)">Megtekintés</v-btn>
-          </td>
-        </tr>
-      </tbody>
-    </v-table>
+      <!-- Popup Modal -->
+      <v-dialog v-model="dialog" max-width="50 vw" theme = "dark">
+        <v-card max-width="50 vw">
+          <v-card-title>Üzenet részletei</v-card-title>
+          <v-card-text>
+            <p><strong>Feladó:</strong> {{ selectedMessage?.senderUserID }}</p>
+            <p><strong>Dátum:</strong> {{ selectedMessage?.date }}</p>
+            <p><strong>Üzenet:</strong> {{ selectedMessage?.message }}</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary" @click="dialog = false">Bezárás</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
+    <div v-else>
+      <h1>Elküldött üzenetek</h1>
+      <v-table theme="dark"  fixed-header style="border-radius: 3%;" class="messageTable">
+        <thead>
+          <tr>
+            
+            <th class="text-center" style="width: 15vw; justify-content: center;">Dátum</th>
+            <th class="text-center" style="width: 15vw; justify-content: center;">Üzenet</th>
+            <th class="text-center" style="width: 15vw; justify-content: center;">Interakció</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="uzenet in data" ><!--:key="uzenet.id"-->
+          
+            <td style="width: 15vw; justify-content: center;">{{ uzenet.date }}</td>
+            <td id="szoveg" style="width: 15vw; justify-content: center;">{{ uzenet.message }}</td>
+            <td style="width: 15vw; justify-content: center;">
+              <v-btn @click="openDialog(uzenet)">Megtekintés</v-btn>
+            </td>
+          </tr>
+        </tbody>
+      </v-table>
 
-    <!-- Popup Modal -->
-    <v-dialog v-model="dialog" max-width="50 vw" theme = "dark">
-      <v-card max-width="50 vw">
-        <v-card-title>Üzenet részletei</v-card-title>
-        <v-card-text>
-          <p><strong>Feladó:</strong> {{ selectedMessage?.senderUserID }}</p>
-          <p><strong>Dátum:</strong> {{ selectedMessage?.date }}</p>
-          <p><strong>Üzenet:</strong> {{ selectedMessage?.message }}</p>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn color="primary" @click="dialog = false">Bezárás</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+      <!-- Popup Modal -->
+      <v-dialog v-model="dialog" max-width="50 vw" theme = "dark">
+        <v-card max-width="50 vw">
+          <v-card-title>Üzenet részletei</v-card-title>
+          <v-card-text>
+            <p><strong>Feladó:</strong> {{ selectedMessage?.senderUserID }}</p>
+            <p><strong>Dátum:</strong> {{ selectedMessage?.date }}</p>
+            <p><strong>Üzenet:</strong> {{ selectedMessage?.message }}</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary" @click="dialog = false">Bezárás</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
+    
 
-    <RouterView />
+    
   </main>
 </template>
 
@@ -64,5 +116,37 @@ const openDialog = (uzenet: Message) => {
   text-overflow: ellipsis;
   width: 15vw;
   display: inline-block;
+}
+
+@media (orientation: portrait) {
+  .messageTable{
+    height: 90vw !important;
+    width: 40vw !important;
+  }
+  .switchBtn{
+    width: 15vw !important;
+    height: 20vw !important;
+  }
+  .navigationdrawer{
+    width: 20vw !important;
+    height: 90vw !important;
+  }
+}
+
+
+/* Landscape mode */
+@media (orientation: landscape) {
+  .messageTable{
+    height: 30vw !important;
+    width: 95vw !important;
+  }
+  .switchBtn{
+    width: 15vw !important;
+    height: 20vw !important;
+  }
+  .navigationdrawer{
+    width: 15vw !important;
+    height: 40vw !important;
+  }
 }
 </style>
