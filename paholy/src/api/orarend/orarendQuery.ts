334a294c87cb5bd69d80a07752a39d526aff5ef0
bUMmrR <agocsdaniel5454@gmail.com>
2025-03-18 19:28:54 +0100

    orarend megjelenités kész, még a hét váltó nem csinál semmit

--- paholy/src/api/orarend/orarendQuery.ts
+++ paholy/src/api/orarend/orarendQuery.ts
@@ -1,26 +1,24 @@
 import axiosClient from "@/lib/axios"
-import { useMutation, useQuery } from "@tanstack/vue-query"
-import { useRoute, useRouter } from "vue-router"
+import { useQuery } from "@tanstack/vue-query"
 import { QUERY_KEYS } from "@/utils/QueryKeys"
-import { jwtDecode } from "jwt-decode";
 import { useCookieHandler } from "@/stores/cookieHandler";
 import type { Lesson } from "./orarend";
 import { useErrorHandler } from "@/stores/errorHandler";
 
-const getOrarend = async (): Promise<Lesson[]> => {
-    const {getCookie} = useCookieHandler()
+const getOrarend = async (weekStart: string): Promise<Lesson[]> => {
+    const { getCookie } = useCookieHandler()
     const config = {
         headers: { Authorization: `Bearer ${getCookie("alap")}` }
     };
-    const response = await axiosClient.get(`http://localhost:3000/orarend`,config) // ${document.cookie}
+    const response = await axiosClient.get(`http://localhost:3000/orarend?weekStart=${weekStart}`, config)
     return response.data
 }
 
-export const useGetOrarend = () => {
-    const {setError} = useErrorHandler()
+export const useGetOrarend = (weekStart: string) => {
+    const { setError } = useErrorHandler()
     const query = useQuery({
-        queryKey: [QUERY_KEYS.getOrarend],
-        queryFn: getOrarend,
+        queryKey: [QUERY_KEYS.getOrarend, weekStart],
+        queryFn: () => getOrarend(weekStart),
     })
 
     if (query.error.value) {
@@ -30,4 +28,11 @@ export const useGetOrarend = () => {
     return query
 }
 
-
+export const fetchOrarend = async (weekStart: string): Promise<Lesson[]> => {
+    try {
+        return await getOrarend(weekStart);
+    } catch (error) {
+        console.error("Lekérdezési hiba:", error);
+        throw error;
+    }
+}
