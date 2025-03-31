1ac18d95af5455dd8bcee565fc252ba269416da3
gitakos <kovacsakos.005@gmail.com>
2025-03-31 11:11:35 +0200

    annyira szeretem a mergelést

--- backend/api/controllers/jegyController.js
+++ backend/api/controllers/jegyController.js
@@ -5,20 +5,14 @@ const lessonService =  require("../services/lessonService")
 
 exports.getJegyek = async (req, res, next) =>
 {
-<<<<<<< HEAD
     console.log("JEGY KEZD")
-=======
->>>>>>> 681559e58d811c33039ca72de8d00085d1452a3b
     const jegyek = await jegyService.getJegyek(req.role.ID)
     res.status(201).json(jegyek);
 }
 
 exports.getJegyekTanar = async (req, res, next) =>
 {
-<<<<<<< HEAD
     console.log("JEGY KEZD")
-=======
->>>>>>> 681559e58d811c33039ca72de8d00085d1452a3b
     const csoportok = await groupService.getTeacherGroups(req.role.ID)
     const jegyek = [];
     for (const element of csoportok) { //csoportok alapján vannak csoportosítva a jegyek, és azon belül tantárgyanként, majd dátum és személy alapján csak frontenden
@@ -33,11 +27,7 @@ exports.getJegyekTanar = async (req, res, next) =>
                 targyAlapjan.push(beszurando);
             }
             const ido = new Date(jegy.date)
-<<<<<<< HEAD
             targyAlapjan[[...targyak].indexOf(jegy.subjectName)].push(jegy);//[ido.getMonth()]
-=======
-            targyAlapjan[[...targyak].indexOf(jegy.subjectName)].push(jegy);
->>>>>>> 681559e58d811c33039ca72de8d00085d1452a3b
         });
     
         const object = {
@@ -47,23 +37,15 @@ exports.getJegyekTanar = async (req, res, next) =>
         };
         jegyek.push(object);
     }
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 681559e58d811c33039ca72de8d00085d1452a3b
     res.status(201).json(jegyek);
 
 }
 
 exports.getJegyekSzulo = async (req, res, next) =>
 {
-<<<<<<< HEAD
     console.log("JEGY KEZD szülő")
     console.log(req.decoded)
     console.log(req.params.id)
-=======
->>>>>>> 681559e58d811c33039ca72de8d00085d1452a3b
     const jegyek = await jegyService.getJegyek(req.params.id)
     res.status(201).json(jegyek);
 }
